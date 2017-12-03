990c21773349217ea5277de34b584b4d3b50d509
joelee <as955133@gmail.com>
2017-12-03 20:01:32 +0800

    不錯玩!!

--- Linq1/Form1.Designer.cs
+++ Linq1/Form1.Designer.cs
@@ -28,12 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnScoreList = new System.Windows.Forms.Button();
+            this.btnSortAsc = new System.Windows.Forms.Button();
+            this.btnSorDesc = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.txtShow = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(509, 70);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(128, 40);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "搜尋";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnScoreList
+            // 
+            this.btnScoreList.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScoreList.Location = new System.Drawing.Point(509, 178);
+            this.btnScoreList.Margin = new System.Windows.Forms.Padding(4);
+            this.btnScoreList.Name = "btnScoreList";
+            this.btnScoreList.Size = new System.Drawing.Size(128, 40);
+            this.btnScoreList.TabIndex = 2;
+            this.btnScoreList.Text = "成績列表";
+            this.btnScoreList.UseVisualStyleBackColor = true;
+            this.btnScoreList.Click += new System.EventHandler(this.btnScoreList_Click);
+            // 
+            // btnSortAsc
+            // 
+            this.btnSortAsc.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSortAsc.Location = new System.Drawing.Point(509, 302);
+            this.btnSortAsc.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSortAsc.Name = "btnSortAsc";
+            this.btnSortAsc.Size = new System.Drawing.Size(128, 40);
+            this.btnSortAsc.TabIndex = 3;
+            this.btnSortAsc.Text = "遞增排序";
+            this.btnSortAsc.UseVisualStyleBackColor = true;
+            this.btnSortAsc.Click += new System.EventHandler(this.btnSortAsc_Click);
+            // 
+            // btnSorDesc
+            // 
+            this.btnSorDesc.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSorDesc.Location = new System.Drawing.Point(509, 428);
+            this.btnSorDesc.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSorDesc.Name = "btnSorDesc";
+            this.btnSorDesc.Size = new System.Drawing.Size(128, 40);
+            this.btnSorDesc.TabIndex = 4;
+            this.btnSorDesc.Text = "遞減排序";
+            this.btnSorDesc.UseVisualStyleBackColor = true;
+            this.btnSorDesc.Click += new System.EventHandler(this.btnSorDesc_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 86);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "查詢成績";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(151, 86);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(316, 25);
+            this.txtSearch.TabIndex = 6;
+            // 
+            // txtShow
+            // 
+            this.txtShow.Location = new System.Drawing.Point(151, 224);
+            this.txtShow.Multiline = true;
+            this.txtShow.Name = "txtShow";
+            this.txtShow.Size = new System.Drawing.Size(299, 254);
+            this.txtShow.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(687, 590);
+            this.Controls.Add(this.txtShow);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSorDesc);
+            this.Controls.Add(this.btnSortAsc);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnScoreList);
+            this.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "LINQ查詢陣列";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnScoreList;
+        private System.Windows.Forms.Button btnSortAsc;
+        private System.Windows.Forms.Button btnSorDesc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.TextBox txtShow;
     }
 }
 
--- Linq1/Form1.cs
+++ Linq1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Linq;
 
 namespace Linq1
 {
@@ -15,6 +16,83 @@ namespace Linq1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        int[] score = new int[] { 23, 45, 100, 87, 90, 65, 44 };
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string s = "";
+            for(int i =0; i<score.Length;i++)
+            {
+                s += score[i] + Environment.NewLine;
+            }
+            txtShow.Text = s;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            int search;
+            try
+            {
+                search = int.Parse(txtSearch.Text);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+            string s = "";
+            var result = from r in score
+                         where r == search
+                         select r;
+
+            if(result.Count()==0)
+            {
+                s = "查無資料";
+            }
+            else
+            {
+                s = "陣列內含";
+                foreach(var r in result)
+                {
+                    s += r;
+                }
+            }
+            txtShow.Text = s;
+        }
+
+        private void btnScoreList_Click(object sender, EventArgs e)
+        {
+            txtShow.Clear();
+            Form1_Load(sender, e);//重複Form1_Load事件
+        }
+
+        private void btnSortAsc_Click(object sender, EventArgs e)
+        {
+            string s = "";
+            var result = from r in score
+                         orderby r
+                         select r;
+            foreach(var r in result)
+            {
+                s += r + Environment.NewLine;
+                //Environment.NewLine
+                //提供有關目前環境和平台的資訊，以及操作的方法。 這個類別無法被繼承。
+            }
+            txtShow.Text = "遞增排序：" + Environment.NewLine + s;
+        }
+
+        private void btnSorDesc_Click(object sender, EventArgs e)
+        {
+            string s = "";
+            var result = from r in score
+                         orderby r descending
+                         select r;
+            foreach(var r in result)
+            {
+                s += r + Environment.NewLine;
+            }
+            txtShow.Text = "遞減排序：" + Environment.NewLine + s;
         }
     }
 }
